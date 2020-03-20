8906c55525e58620ce4973b81e26d722dfee265e
Silvia <silvia.tulli.92@gmail.com>
2020-03-20 09:52:34 +0100

    Add files via upload

---
+++ minmax/src/minmax.py
@@ -0,0 +1,55 @@
+from game_model import GameState
+
+class Node(GameState):
+    def __init__(self, is_child_turn=None, balls=None):
+        if is_child_turn is None:
+            super(Node, self).__init__()
+        else:
+            self.is_child_turn = is_child_turn
+            self.balls = balls
+            
+    def score(self):
+        if not self.is_child_turn and self.isFinished():
+            return 1
+        elif self.is_child_turn and self.isFinished():
+            return -1
+        else:
+            return 0
+    
+    def children(self):
+        # [Node(), ..., Node()]
+        nodes = list()
+
+        #populate tree
+        for valid_action in self.valid_actions():
+            new_state = self.make_action(valid_action[0], valid_action[1])
+            node = Node(new_state.is_child_turn, new_state.balls)
+            nodes.append(node)
+            
+        return nodes
+
+def minimax(node, depth):
+    score = node.score()
+    children = node.children()
+    isFinished = node.isFinished()
+    maximizingPlayer = node.is_child_turn
+
+    if depth == 0 or isFinished:
+        return score
+
+    if maximizingPlayer:
+        value = -1
+        for child in children:
+            value = max(value, minimax(child, depth - 1))
+        return value
+    else:
+        value = 1
+        for child in children:
+            value = min(value, minimax(child, depth - 1))
+        return value
+
+
+if __name__ == "__main__":
+    node = Node()
+    value = minimax(node, 10)
+    print(value)
