921715a71e7f3eba5a9dcb6af52f5603e92f58a6
FirefoxMetzger <sebastian@wallkoetter.net>
2020-04-08 20:09:42 +0200

    PEP compliant formatting + remove debug prints

--- minmax/src/game_manager.py
+++ minmax/src/game_manager.py
@@ -5,34 +5,31 @@ from minmax import Q
 from interface import View
 from robot_decision import Robot
 from child_decision import Child
-#from robot_manager import RobotManager
+# from robot_manager import RobotManager
 import time
 
 
-def play_game(robot,child):
+def play_game(robot, child):
     state = GameState()
     visualization = View()
     num_actions = 0
 
     while not state.isFinished():
         visualization.update(state)
-        #time.sleep(0.5)
-        
+        # time.sleep(0.5)
+
         valid_actions = state.valid_actions()
-        
+
         if state.is_child_turn:
             action, ball_id = child.policy(state)
             num_actions += 1
-
 
         else:
             action, ball_id = robot.policy(state)
             robot.give_explanation((action, ball_id), state)
 
-            #random_action
-            #worst_action
-
         old_state = state
+        print(GameState.get_state_id(state))
         state = state.make_action(action, ball_id)
         child.update(old_state, (action, ball_id), state)
 
@@ -43,11 +40,11 @@ def play_game(robot,child):
         outcome = 'Failure'
     elif not state.is_child_turn and state.isFinished():
         outcome = 'Success'
-    
+
     print(outcome)
     print(num_actions)
-
     return outcome, num_actions
+
 
 if __name__ == "__main__":
     number_of_games = 3
@@ -56,6 +53,5 @@ if __name__ == "__main__":
     # robot = RobotManager()
 
     for game in range(number_of_games):
-        outcome, num_actions = play_game(robot,child)
+        outcome, num_actions = play_game(robot, child)
         robot.update_POMDP(outcome, num_actions)
-        
--- minmax/src/minmax.py
+++ minmax/src/minmax.py
@@ -1,5 +1,6 @@
 from game_model import GameState
 
+
 class Node(GameState):
     def __init__(self, is_child_turn=None, balls=None):
         if is_child_turn is None:
@@ -7,7 +8,7 @@ class Node(GameState):
         else:
             self.is_child_turn = is_child_turn
             self.balls = balls
-            
+
     def score(self):
         if not self.is_child_turn and self.isFinished():
             return 1
@@ -15,17 +16,18 @@ class Node(GameState):
             return -1
         else:
             return 0
-    
+
     def children(self):
         nodes = list()
 
-        #populate tree
+        # populate tree
         for valid_action in self.valid_actions():
             new_state = self.make_action(valid_action[0], valid_action[1])
             node = Node(new_state.is_child_turn, new_state.balls)
             nodes.append(node)
-            
+
         return nodes
+
 
 def minimax(node, depth):
     score = node.score()
@@ -47,17 +49,20 @@ def minimax(node, depth):
             value = min(value, minimax(child, depth - 1))
         return value
 
+
 def V(state):
     node = Node(is_child_turn=state.is_child_turn, balls=state.balls)
     value = minimax(node, 3)
     return value
+
 
 def Q(state, action):
     action, ball_id = action
     new_state = state.make_action(action, ball_id)
     q_value = V(new_state)
     return q_value
-    
+
+
 if __name__ == "__main__":
     node = Node()
     value = minimax(node, 3)
