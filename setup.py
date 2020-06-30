28bd8b20e788416f972522a6beeac15f4c9dd2fd
FirefoxMetzger <sebastian@wallkoetter.net>
2020-06-30 15:15:50 +0200

    initial commit

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='minmax',
+    version='0.0.0',
+    url='https://github.com/Silviatulli/minmax',
+    author='Silvia Tulli',
+    author_email='TODO',
+    description='Code for the AAAI2021 project.',
+    packages=find_packages(),
+    install_requires=[
+        'numpy >= 1.18.2',
+        'matplotlib >= 3.2.1'
+    ]
+)
