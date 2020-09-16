7eb1e59d7c2e6ff0378f965d21d28d086888f56e
silviatulli <silvia.tulli.92@gmail.com>
2020-09-16 17:09:31 +0200

    removing duplicates in other_actions method

--- minmax/robot_decision.py
+++ minmax/robot_decision.py
@@ -3,16 +3,14 @@ import numpy as np
 from minmax import GameState
 from minmax.minmaxSearch import Q
 from copy import deepcopy
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import matplotlib.style
 
 
 class Robot(object):
-    # TODO: action selection - policy
-    # get child action and update POMDP
-    # generate an explanation
 
     def policy(self, state):
-        # given the current state of the game makes the robot selects
-        # an action in the game
         valid_actions = state.valid_actions()
         best_action = valid_actions[0]
         
@@ -36,45 +34,9 @@ class Robot(object):
 
 
     def update_POMDP(self, outcome, num_actions):
-        # changes the POMPD based on the child actions
         return
 
     def give_demonstration(self, action, state):
-        # given the current state of the child makes the robot selects
-        # an action/explanation count number of turns
-
-        # TODO: transform the explanation into a demonstration
-        # consider state, action and reward
-        # reward function - modify here and in the child_evaluation
-
-        # make the reward function more sophisticated
-        # from the current state, perform an action and store the reward
-        # use the minmax to compute the reward that you could get from performing a bunch of other actions (3(?))
-        # evaluate the goodness of the performed action giving a reward with respect to the other action outputs
-
-        old_score = state.get_score('child')
-        new_state = deepcopy(state)
-        new_state.make_action(action)
-
-        reward = old_score - state.get_score('child')
-
-        return (state, action, reward, new_state)
-
-
-    def give_other_actions(self, action, state):
-
-    # given the current state of the child makes the robot selects
-        # an action/explanation count number of turns
-
-        # TODO: transform the explanation into a demonstration
-        # consider state, action and reward
-        # reward function - modify here and in the child_evaluation
-
-        # make the reward function more sophisticated
-        # from the current state, perform an action and store the reward
-        # use the minmax to compute the reward that you could get from performing a bunch of other actions (3(?))
-        # evaluate the goodness of the performed action giving a reward with respect to the other action outputs
-
         old_score = state.get_score('child')
         new_state = deepcopy(state)
         new_state.make_action(action)
@@ -109,17 +71,6 @@ class Robot(object):
         else:
             other_actions = valid_actions
 
-    
-        ## How do you deal with the opponent goal? Do the rewards make sense for the child? How? How do you reason about the robot action?
-        # when the robot makes an action the child should updates his q-table
-        # link child current state with child next state -
-        # How do you generalize over actions that are bad? - check work about reasoning over opponent action 
-        # (((Consider a different scenario where the agents are cooperating))) 
-
-        # TODO: generate a NL string in the form of "If I would take action A instead of action B I would get this amount ---"
-        # check f-string
-        #print(f"By taking this action I get {current_score} points, while by doing {other_action[0]} I would get {rewards[0]} of difference")
-
 
         scores = np.array(scores)
         rewards = current_score - scores
@@ -129,15 +80,12 @@ class Robot(object):
         other_actions_matrix = np.column_stack((other_actions, rewards))
         print(other_actions, rewards)
 
+
         return  other_actions_matrix
 
-### when is it necessary to give an explaination and when is it better to give examples?
-## how many sample h
 
-    def give_explanation(self):
+    def give_examples(self):
         array_shape = 12*12*12*12*2
-        ## select states where the difference between the child q values and robot q values
-        ## curriculum learning 
         valid_states = []
         num_examples = 10
         while len(valid_states) < num_examples:
@@ -161,11 +109,6 @@ class Robot(object):
             best_action = self.policy(robot_state)
             new_state, reward, done, info = robot_state.make_action(best_action)
             reward = robot_state.get_score('child') - new_state.get_score('child')
-            # if robot_state.get_score('child') < new_state.get_score('child'):
-            #     reward = 1
-            # else:
-            #     reward = -1
-
             example = (robot_state, best_action, reward, new_state)
             examples.append(example)
 
