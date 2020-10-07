e37a258b2e0efe6e22249380590d9d380c45428e
FirefoxMetzger <sebastian@wallkoetter.net>
2020-10-07 08:51:13 +0200

    convert code to OOP

--- hexgame/play_hex.py
+++ hexgame/play_hex.py
@@ -9,175 +9,167 @@ import gym
 import random
 
 
-def draw_stone(surface, pos, player, config):
-    radius = 0.6 * float(config["GameWindow"]["hexagon_radius"])
+class Hexagon(object):
+    def __init__(self, config, offset=(0, 0)):
+        self.hex_config = config["Hexagon"]
+        self.stone_config = config["Stone"]
+        radius = float(self.hex_config["radius"])
 
-    if player == player.BLACK:
-        color = pygame.Color(config["GameWindow"]["player1_stone_color"])
-    else:
-        color = pygame.Color(config["GameWindow"]["player2_stone_color"])
+        offset = np.asarray(offset).reshape((-1, 1))
+        rot_matrix = np.array([
+            [cos(pi / 3), -sin(pi / 3)],
+            [sin(pi / 3), cos(pi / 3)]
+        ])
+        vector = np.array([[radius], [0]])
 
-    pos = (int(np.round(pos[0])), int(np.round(pos[1])))
-    pygame.draw.circle(surface, color, pos, int(np.round(radius)))
-    border_color = pygame.Color(config["GameWindow"]["stone_border_color"])
-    pygame.draw.circle(surface, border_color, pos, int(np.round(radius)), 1)
+        points_pos = list()
+        points_neg = list()
+        for _ in range(3):
+            points_pos.append((vector + offset).ravel().tolist())
+            points_neg.append((offset - vector).ravel().tolist())
+            vector = np.matmul(rot_matrix, vector)
 
+        self.points = points_pos + points_neg
+        self.midpoint = offset
 
-def hexagon_points(radius, offset=(0, 0)):
-    offset = np.asarray(offset).reshape((-1, 1))
-    rot_matrix = np.array([
-        [cos(pi / 3), -sin(pi / 3)],
-        [sin(pi / 3), cos(pi / 3)]
-    ])
-    vector = np.array([[radius], [0]])
+    def draw(self, surface, mouse_pos, stone=player.EMPTY):
+        if self.contains(mouse_pos):
+            color = pygame.Color(self.hex_config["primary_color"])
+        else:
+            color = pygame.Color(self.hex_config["color"])
+        pygame.draw.polygon(surface, color, self.points)
 
-    points_pos = list()
-    points_neg = list()
-    for _ in range(3):
-        points_pos.append((vector + offset).ravel().tolist())
-        points_neg.append((offset - vector).ravel().tolist())
-        vector = np.matmul(rot_matrix, vector)
+        border_color = pygame.Color(self.hex_config["border_color"])
+        pygame.draw.polygon(surface, border_color, self.points, 3)
 
-    return points_pos + points_neg
+        if stone == player.EMPTY:
+            return
+        elif stone == player.WHITE:
+            color = pygame.Color(self.stone_config["player2_color"])
+        elif stone == player.BLACK:
+            color = pygame.Color(self.stone_config["player1_color"])
 
+        radius = int(self.stone_config["radius"])
+        x = int(np.round(self.midpoint[0]))
+        y = int(np.round(self.midpoint[1]))
+        pygame.draw.circle(surface, color, (x, y), int(np.round(radius)))
 
-def test_line(pointA, pointB, test_point):
-    x1, y1 = pointA
-    x2, y2 = pointB
-    x, y = test_point
+        border_color = pygame.Color(self.stone_config["border_color"])
+        pygame.draw.circle(surface, border_color, (x, y), radius, 1)
 
-    return (x - x1) * (y2 - y1) - (y - y1) * (x2 - x1)
+    def contains(self, point):
+        radius = int(self.hex_config["radius"])
+        mx, my = self.midpoint
+        x, y = point
 
-
-def inside_hexagon(midpoint, radius, test_point):
-    mx, my = midpoint
-    x, y = test_point
-
-    # outside envelopping circle
-    if np.sqrt((x - mx)**2 + (y - my)**2) > radius:
-        return False
-
-    # inside containing circle
-    if np.sqrt((x - mx)**2 + (y - my)**2) < radius * np.sqrt(3) / 2:
-        return True
-
-    # between enveloping and containing circles
-    # perform generic polygon test
-    points = hexagon_points(radius, midpoint)
-    sign = np.sign(test_line(points[0], points[1], test_point))
-    for idx in range(1, len(points) - 1):
-        pointA = points[idx]
-        pointB = points[idx + 1]
-        val = np.sign(test_line(pointA, pointB, test_point))
-        if sign != val:
+        # outside envelopping circle
+        if np.sqrt((x - mx)**2 + (y - my)**2) > radius:
             return False
 
-    return True
+        # inside containing circle
+        if np.sqrt((x - mx)**2 + (y - my)**2) < radius * np.sqrt(3) / 2:
+            return True
+
+        # between enveloping and containing circles
+        # perform generic polygon test
+        points = self.points
+        sign = np.sign(self._test_line(points[0], points[1], point))
+        for idx in range(1, len(points) - 1):
+            pointA = points[idx]
+            pointB = points[idx + 1]
+            val = np.sign(self._test_line(pointA, pointB, point))
+            if sign != val:
+                return False
+
+        return True
+
+    def _test_line(self, pointA, pointB, test_point):
+        x1, y1 = pointA
+        x2, y2 = pointB
+        x, y = test_point
+
+        return (x - x1) * (y2 - y1) - (y - y1) * (x2 - x1)
 
 
-def pos2idx(test_point, config):
-    positions = board_positions(config)
-    radius = int(config["GameWindow"]["hexagon_radius"])
+class Board(object):
+    def __init__(self, config):
+        self.centers = self.board_positions(config)
+        self.hexagons = list()
+        self.config = config
+        for pos in self.centers:
+            self.hexagons.append(Hexagon(config, offset=pos))
 
-    for idx, pos in enumerate(positions):
-        if inside_hexagon(pos, radius, test_point):
-            return idx
+    def draw(self, surface, sim, mouse_pos):
+        occupancy = sim.board.ravel()
 
-    return None
+        bg_color = pygame.Color(self.config["GameWindow"]["background_color"])
+        surface.fill(bg_color)
 
+        self.draw_game_border(surface)
 
-def board_positions(config):
-    game_conf = config["GameWindow"]
-    long_radius = int(game_conf["hexagon_radius"])
-    short_radius = np.sqrt(3) / 2 * long_radius
-    board_size = int(config["HexGame"]["board_size"])
+        for stone, hexagon in zip(occupancy, self.hexagons):
+            if hexagon.contains(mouse_pos):
+                hexagon.hovered = True
+            else:
+                hexagon.hovered = False
 
-    col_px = [idx * 1.5 * long_radius + long_radius
-              for idx in range(2 * board_size - 1)]
-    row_px = [(idx + 1) * short_radius
-              for idx in range(2 * board_size - 1)]
+            hexagon.draw(surface, mouse_pos, stone)
 
-    stones_per_row = ([idx for idx in range(1, board_size)] +
-                      [idx for idx in range(board_size, 0, -1)])
-    start_idx = ([idx for idx in range(board_size - 1, 0, -1)] +
-                 [idx for idx in range(board_size)])
+    def draw_game_border(self, surface):
+        long_radius = int(self.config["GameWindow"]["hexagon_radius"])
+        short_radius = np.sqrt(3) / 2 * long_radius
+        board_size = int(config["HexGame"]["board_size"])
 
-    center_positions = list()
-    for offset in range(board_size):
-        y_start = board_size - 1 + offset
-        x_start = offset
-        for row_counter in range(board_size):
-            y_idx = y_start - row_counter
-            x_idx = x_start + row_counter
-            center_positions.append((col_px[x_idx], row_px[y_idx]))
+        positions = self.centers
 
-    return center_positions
+        left = (positions[0][0] - 2.5 * long_radius, positions[0][1])
+        right = (positions[-1][0] + 2.5 * long_radius, positions[-1][1])
+        top = (positions[board_size - 1][0],
+               positions[board_size - 1][1] - 2 * long_radius)
+        bottom = (positions[board_size - 1][0],
+                  positions[(board_size - 1) * board_size][1] + 2 * long_radius)
+        center = positions[len(positions) // 2]
 
+        color = pygame.Color(config["GameWindow"]["player1_stone_color"])
+        pygame.draw.polygon(surface, color, [left, top, center])
+        pygame.draw.polygon(surface, color, [center, bottom, right])
 
-def draw_empty_board(surface, hover_idx, config):
-    game_conf = config["GameWindow"]
-    surface.fill(pygame.Color(game_conf["background_color"]))
+        color = pygame.Color(config["GameWindow"]["player2_stone_color"])
+        pygame.draw.polygon(surface, color, [top, right, center])
+        pygame.draw.polygon(surface, color, [bottom, left, center])
 
-    long_radius = int(game_conf["hexagon_radius"])
-    short_radius = np.sqrt(3) / 2 * long_radius
-    board_size = int(config["HexGame"]["board_size"])
+    def pos2idx(self, test_point):
+        for idx, hexagon in enumerate(self.hexagons):
+            if hexagon.contains(test_point):
+                return idx
+        return None
 
-    positions = board_positions(config)
+    def board_positions(self, config):
+        game_conf = config["GameWindow"]
+        long_radius = int(game_conf["hexagon_radius"])
+        short_radius = np.sqrt(3) / 2 * long_radius
+        board_size = int(config["HexGame"]["board_size"])
 
-    for idx, pos in enumerate(positions):
-        edges = hexagon_points(long_radius, pos)
-        if hover_idx == idx:
-            pygame.draw.polygon(surface, pygame.Color(
-                game_conf["hexagon_accent_color"]), edges)
-        else:
-            pygame.draw.polygon(surface, pygame.Color(
-                game_conf["hexagon_color"]), edges)
-        pygame.draw.polygon(surface, pygame.Color(
-            game_conf["hexagon_border_color"]), edges, 3)
+        col_px = [idx * 1.5 * long_radius + long_radius
+                  for idx in range(2 * board_size - 1)]
+        row_px = [(idx + 1) * short_radius
+                  for idx in range(2 * board_size - 1)]
 
+        stones_per_row = ([idx for idx in range(1, board_size)] +
+                          [idx for idx in range(board_size, 0, -1)])
+        start_idx = ([idx for idx in range(board_size - 1, 0, -1)] +
+                     [idx for idx in range(board_size)])
 
-def draw_env(surface, sim, hover_idx, config):
-    game_conf = config["GameWindow"]
-    surface.fill(pygame.Color(game_conf["background_color"]))
+        center_positions = list()
+        for offset in range(board_size):
+            y_start = board_size - 1 + offset
+            x_start = offset
+            for row_counter in range(board_size):
+                y_idx = y_start - row_counter
+                x_idx = x_start + row_counter
+                center_positions.append((col_px[x_idx], row_px[y_idx]))
 
-    long_radius = int(game_conf["hexagon_radius"])
-    short_radius = np.sqrt(3) / 2 * long_radius
-    board_size = int(config["HexGame"]["board_size"])
-
-    positions = board_positions(config)
-    occupancy = sim.board.ravel()
-
-    left = (positions[0][0] - 2.5 * long_radius, positions[0][1])
-    right = (positions[-1][0] + 2.5 * long_radius, positions[-1][1])
-    top = (positions[board_size - 1][0],
-           positions[board_size - 1][1] - 2 * long_radius)
-    bottom = (positions[board_size - 1][0],
-              positions[(board_size - 1) * board_size][1] + 2 * long_radius)
-    center = positions[len(positions) // 2]
-
-    color = pygame.Color(config["GameWindow"]["player1_stone_color"])
-    pygame.draw.polygon(surface, color, [left, top, center])
-    pygame.draw.polygon(surface, color, [center, bottom, right])
-
-    color = pygame.Color(config["GameWindow"]["player2_stone_color"])
-    pygame.draw.polygon(surface, color, [top, right, center])
-    pygame.draw.polygon(surface, color, [bottom, left, center])
-
-    for idx, pos in enumerate(positions):
-        edges = hexagon_points(long_radius, pos)
-        if hover_idx == idx:
-            pygame.draw.polygon(surface, pygame.Color(
-                game_conf["hexagon_accent_color"]), edges)
-        else:
-            pygame.draw.polygon(surface, pygame.Color(
-                game_conf["hexagon_color"]), edges)
-        pygame.draw.polygon(surface, pygame.Color(
-            game_conf["hexagon_border_color"]), edges, 3)
-
-        if occupancy[idx] == player.BLACK:
-            draw_stone(surface, pos, player.BLACK, config)
-        elif occupancy[idx] == player.WHITE:
-            draw_stone(surface, pos, player.WHITE, config)
+        return center_positions
 
 
 def main(config):
@@ -198,11 +190,13 @@ def main(config):
                    board_size=board_size)
     state, info = env.reset()
 
+    board = Board(config)
+
     done = False
     running = True
     while running:
         mouse_pos = pygame.mouse.get_pos()
-        hover_idx = pos2idx(mouse_pos, config)
+        hover_idx = board.pos2idx(mouse_pos)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -222,7 +216,7 @@ def main(config):
         if done:
             state, info = env.reset()
 
-        draw_env(surface, env.simulator, hover_idx, config)
+        board.draw(surface, env.simulator, mouse_pos)
         pygame.display.update()
 
 
